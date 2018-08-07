01acd71340245392454632389fd11d7362d6089d
maxiaogang <maxiaogang@youzan.com>
2018-08-07 09:23:06 +0800

    添加字符串相关操作

---
+++ src/com/ma/learn/stringhandling/TrimDemo.java
@@ -0,0 +1,10 @@
+package com.ma.learn.stringhandling;
+
+public class TrimDemo {
+
+  public static void main(String[] args) {
+    String s = "     hello world      ".trim();
+    System.out.println(s);
+  }
+
+}
